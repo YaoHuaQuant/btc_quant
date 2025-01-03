72ce300ba8a08f194e53104b5278d5b2c63039d1
Gx <475444812@qq.com>
2025-01-04 04:02:42 +0800

    实现最基础的波动率策略，0.2%利润率，没有bubble

--- src/strategy/maker_only_volatility_strategy.py
+++ src/strategy/maker_only_volatility_strategy.py
@@ -2,6 +2,7 @@
 只做Maker的做多波动率交易策略
 深度左侧交易
 """
+import logging
 import math
 from typing import Dict
 
@@ -17,16 +18,16 @@ class MakerOnlyLongOnlyVolatilityStrategy(StrategyInterface):
     只做多策略
     """
     # 定义参数
-    CASH_SLOT_NUM = 500  # 资产分割粒度
-    OPEN_PRICE_SLOT_NUM = 400  # 开仓挂单价位粒度
+    CASH_SLOT_NUM = 1000  # 资产分割粒度
+    OPEN_PRICE_SLOT_NUM = 800  # 开仓挂单价位粒度
     # PERCENTAGE_MAX_OPEN_PRICE = 0.90  # 开仓最高价格（顶部的90%）
     PERCENTAGE_MAX_OPEN_PRICE = 0.99  # 开仓最高价格（顶部的90%）
     # PERCENTAGE_MIN_OPEN_PRICE = 0.50  # 开仓最低价格（顶部的50%）
     PERCENTAGE_MIN_OPEN_PRICE = 0.59  # 开仓最低价格（顶部的50%）
     # PERCENTAGE_MAX_CLOSE_PRICE = 0.95  # 平仓最高价格（顶部的95%）
-    PERCENTAGE_MAX_CLOSE_PRICE = 1.05  # 平仓最高价格（顶部的95%）
+    PERCENTAGE_MAX_CLOSE_PRICE = 1.04  # 平仓最高价格（顶部的95%）
     # PERCENTAGE_MIN_CLOSE_PRICE = 0.50  # 平仓最低价格（顶部的50%）
-    PERCENTAGE_MIN_CLOSE_PRICE = 0.60  # 平仓最低价格（顶部的50%）
+    PERCENTAGE_MIN_CLOSE_PRICE = 0.59  # 平仓最低价格（顶部的50%）
     PERCENTAGE_MINIMUM_PROFIT = 0.002  # 最低利润百分比
     OPENING_ORDER_NUM = 20  # 盘口附近的开单数量
     CLOSING_ORDER_NUM = 20  # 盘口附近的平单数量
@@ -54,11 +55,34 @@ class MakerOnlyLongOnlyVolatilityStrategy(StrategyInterface):
 
         self.data: None | MyOrderArrayTriple = None
 
-        self.open_order_price_dict = dict()  # 保存挂单的开单价格 key为价格 value为order对象
-        self.close_order_price_dict = dict()  # 保存挂单的平单 key为价格 value为order对象
+        self.open_order_price_dict = dict()  # 价格->bt.order映射，用于判断每个价位是否已有订单: key为价格 value为order对象
+        # self.open_order_dict = dict()  # bt.order->MyOrderPair对象映射，用于统计结果：key为bt.order对象 value为MyOrderPair对象
+        self.close_order_price_dict = dict()  # 价格->bt.order映射，用于判断每个价位是否已有订单: key为价格 value为order对象
+        # self.close_order_dict = dict()  # bt.order->MyOrderPair对象映射，用于统计结果：key为bt.order对象 value为MyOrderPair对象
+        self.price_map_close2open = dict()  # close价格->open价格映射，用于查找对应close价格的open成本价格
         self.closed_order_list = []  # 保存已完成的订单 MyOrderPair类型 用于数据统计
 
         self.strategy_adaptor = StrategyAdaptor()
+
+        # 分析数据
+        ## 挂单数据
+        self.opening_amount: float = 0  # 开仓挂单BTC总量
+        self.opening_value: float = 0  # 开仓挂单BTC总价
+        self.closing_amount: float = 0  # 平仓挂单BTC总量
+        self.closing_value: float = 0  # 平仓挂单BTC总价(期望持仓市值)
+        self.closing_cost: float = 0  # 平仓挂单BTC成本 = sum(每单BTC总量 * 每单开仓挂单价格)
+        ## 成交数据
+        self.opening_amount_finished: float = 0  # 已成交的开仓挂单BTC总量
+        self.opening_value_finished: float = 0  # 已成交的开仓挂单BTC总价
+        self.closing_amount_finished: float = 0  # 已成交的平仓挂单BTC总量
+        self.closing_value_finished: float = 0  # 已成交的平仓挂单BTC总价
+        self.closing_cost_finished: float = 0  # 已成交的平仓挂单BTC成本 = sum(每单BTC总量 * 每单开仓挂单价格)
+        ## 期望数据
+        # self.expected_profit: float = 0  # 期望未成交收益 = 平仓挂单BTC总价 - 平仓挂单BTC成本
+        # self.expected_holding_value: float = 0  # 期望持仓市值 = 平仓挂单BTC总价
+        # self.expected_total_value: float = 0  # 期望总资产 = 现金余额 + 期望持仓市值（平仓挂单BTC总价）
+        # self.not_yet_achieved_profit: float = 0  # 尚未达到的收益 = 平仓挂单BTC总价 - 持仓市值
+        # self.actual_profit: float = 0  # 实际已成交收益 = 已成交的平仓挂单BTC总价 - 已成交的平仓挂单BTC成本
 
     def next(self):
         """
@@ -85,7 +109,7 @@ class MakerOnlyLongOnlyVolatilityStrategy(StrategyInterface):
                 self.data.add_open_order(
                     open_price=open_price,
                     close_price=open_price,
-                    quantity=0 # quantity在正式挂单时决定
+                    quantity=0  # quantity在正式挂单时决定
                 )
             ## 在盘口价下方挂上一定数量的开单
             for i in range(open_position, min(open_position + self.OPENING_ORDER_NUM, self.OPEN_PRICE_SLOT_NUM)):
@@ -100,10 +124,13 @@ class MakerOnlyLongOnlyVolatilityStrategy(StrategyInterface):
                         size=open_quantity,
                         exectype=bt.Order.Limit
                     )
+                    # 用于数据统计
+                    self.analysis_add_open_order(open_price=open_price, quantity=open_quantity)
                     # logging.info(f"Order Placed\tDirection: Buy,\tPrice: {new_open_order.price},\tSize: {new_open_order.size}")
                     # self.strategy_adaptor.bind(open_order, MyOrderPairObserver(strategy=self.super_strategy, bt_order=new_open_order))  # 将new_open_order委托给strategy_adaptor， 自动完成价格调整
                     self.open_order_price_dict[open_price] = new_open_order
-        self.debug_log()
+        # todo 向上挂满平单
+        # self.debug_log()
 
     def debug_log(self):
 
@@ -176,24 +203,60 @@ class MakerOnlyLongOnlyVolatilityStrategy(StrategyInterface):
                 ## 调整self.data
                 open_order_array = self.data.open_order_array
                 close_order_array = self.data.close_order_array
-                tmp_order = open_order_array.pop_by_price(open_price)
-                tmp_order.update_status_closing()
+                # tmp_order = open_order_array.pop_by_price(open_price)
+                # tmp_order.update_status_closing()
                 if self.direction == 'long':
                     close_price = open_price * (1 + self.PERCENTAGE_MINIMUM_PROFIT)
                 else:
                     close_price = open_price * (1 - self.PERCENTAGE_MINIMUM_PROFIT)
-                tmp_order.update_close_price(close_price)
-                (is_success, changed_list, pop_order) = close_order_array.add_order(order=tmp_order)
+                close_position = close_order_array.get_position_by_price(close_price)
+
+                # # 向上找一个空的slot挂close单
+                # while close_position <= self.CLOSE_PRICE_SLOT_NUM:
+                #     close_price = close_order_array.get_price_by_position(close_position)
+                #     if not self.close_order_price_dict.__contains__(close_price):
+                #         break
+                #     close_position += 1
+
+                # tmp_order.update_close_price(close_price)
+                # (is_success, changed_list, pop_order) = close_order_array.add_order(order=tmp_order)
                 ### todo add order过程中如果有订单价格发生改变 需要调整已经挂好的订单
                 ### todo 如果close挂满 直接将顶部pop出来的close订单 用市价成交
 
                 new_close_order = self.super_strategy.sell(
                     price=close_price,
-                    size=tmp_order.quantity,
+                    size=order.size,
                     exectype=bt.Order.Limit
                 )
+
+                # if self.close_order_price_dict.__contains__(close_price):
+                #     old_close_order: bt.Order = self.close_order_price_dict.pop(close_price)
+                #     # 市价平掉旧order
+                #     self.super_strategy.sell(
+                #         size=old_close_order.size,
+                #     )
+                #     # 取消旧order挂单
+                #     self.super_strategy.cancel(old_close_order)
+                #     # logging.info(f"Order Market Sell,\tPrice: {new_close_order.price},\tSize: {new_close_order.size}")
+                #
+                #     ## 数据统计用 - START
+                #     self.analysis_remove_close_order(
+                #         close_price=close_price, quantity=old_close_order.size, market_price=self.get_price()
+                #     )
+                #     self.analysis_add_closed_order(
+                #         close_price=close_price, quantity=old_close_order.size, market_price=self.get_price()
+                #     )
+                #     ## 数据统计用 - END
+
+                self.close_order_price_dict[close_price] = new_close_order
                 # logging.info(f"Order Placed\tDirection: Sell,\tPrice: {new_close_order.price},\tSize: {new_close_order.size}")
                 # self.strategy_adaptor.bind(tmp_order, MyOrderPairObserver(strategy=self.super_strategy, bt_order=new_close_order))  # 将new_close_order委托给strategy_adaptor， 自动完成价格调整
+
+                ## 数据统计用 - START
+                self.price_map_close2open[close_price] = open_price
+                self.analysis_remove_open_order(close_price=close_price, quantity=order.size)
+                self.analysis_add_close_order(close_price=close_price, quantity=order.size)
+                ## 数据统计用 - END
 
                 # ## 在盘口价上方挂上一定数量的平单
                 # ### 清除旧挂单
@@ -230,7 +293,10 @@ class MakerOnlyLongOnlyVolatilityStrategy(StrategyInterface):
                 #         self.close_order_price_dict[close_price] = new_close_order
             elif order.issell():
                 # todo 平单完成 保存订单
-                pass
+                # 数据统计用
+                if order.price is not None:
+                    self.analysis_remove_close_order(close_price=order.price, quantity=order.size)
+                    self.analysis_add_closed_order(close_price=order.price, quantity=order.size)
                 ## 调整self.data
                 # close_price = order.price
                 # if close_price is not None:
@@ -246,8 +312,74 @@ class MakerOnlyLongOnlyVolatilityStrategy(StrategyInterface):
         # elif order.status in [order.Rejected]:
         #     logging.info(f"Order Rejected\tDirection: {direction},\tPrice: {order.price},\tSize: {order.size}")
 
+    def analysis_add_open_order(self, open_price: float, quantity: float):
+        """用于分析"""
+        ## 挂单数据
+        self.opening_amount += abs(quantity)  # 开仓挂单BTC总量
+        self.opening_value += abs(quantity) * open_price  # 开仓挂单BTC总价
+
+    def analysis_remove_open_order(self, close_price: float, quantity: float):
+        """用于分析"""
+        open_price = self.price_map_close2open[close_price]
+        ## 挂单数据
+        self.opening_amount -= abs(quantity)  # 开仓挂单BTC总量
+        self.opening_value -= abs(quantity) * open_price  # 开仓挂单BTC总价
+        ## 成交数据
+        self.opening_amount_finished += abs(quantity)  # 已成交的开仓挂单BTC总量
+        self.opening_value_finished += abs(quantity) * open_price  # 已成交的开仓挂单BTC总价
+
+    def analysis_add_close_order(self, close_price: float, quantity: float):
+        """用于分析"""
+        open_price = self.price_map_close2open[close_price]
+        ## 挂单数据
+        self.closing_amount += abs(quantity)  # 平仓挂单BTC总量
+        self.closing_value += abs(quantity) * close_price  # 平仓挂单BTC总价(期望持仓市值)
+        self.closing_cost += abs(quantity) * open_price  # 平仓挂单BTC成本 = sum(每单BTC总量 * 每单开仓挂单价格)
+
+    def analysis_remove_close_order(self, close_price: float, quantity: float, market_price: float = None):
+        """用于分析"""
+        open_price = self.price_map_close2open[close_price]
+        if market_price is not None:
+            close_price_finish = market_price
+        else:
+            close_price_finish = close_price
+        ## 挂单数据
+        self.closing_amount -= abs(quantity)  # 平仓挂单BTC总量
+        self.closing_value -= abs(quantity) * close_price  # 平仓挂单BTC总价(期望持仓市值)
+        self.closing_cost -= abs(quantity) * open_price  # 平仓挂单BTC成本 = sum(每单BTC总量 * 每单开仓挂单价格)
+        ## 成交数据
+        self.closing_amount_finished += abs(quantity)  # 已成交的平仓挂单BTC总量
+        self.closing_value_finished += abs(quantity) * close_price_finish  # 已成交的平仓挂单BTC总价
+        self.closing_cost_finished += abs(quantity) * open_price  # 已成交的平仓挂单BTC成本 = sum(每单BTC总量 * 每单开仓挂单价格)
+
+    def analysis_add_closed_order(self, close_price: float, quantity: float, market_price: float = None):
+        """用于分析"""
+        open_price = self.price_map_close2open[close_price]
+        if market_price is not None:
+            close_price = market_price
+        order_pair = MyOrderPair(
+            open_price=open_price, close_price=close_price,
+            quantity=abs(quantity),
+            commission_rate=self.commission_rate
+        )
+        order_pair.update_status_closing()
+        order_pair.update_status_closed()
+        self.closed_order_list.append(order_pair)
+
     def __repr__(self):
-        return f"{self.super_strategy.data.datetime.date()} {self.super_strategy.data.datetime.time()}\tBTC价格:{self.get_price()}\t现金余额:{self.get_cash()}\t持仓BTC数量:{self.get_position_size()}\t持仓市值:{self.get_holding_value()}\t总资产:{self.get_total_value()}"
+        expected_holding_value = self.get_expected_holding_value()  # 期望持仓市值
+        expected_total_value = self.get_expected_total_value()  # 期望总资产
+        expected_profit = self.get_expected_profit()  # 期望未成交收益
+        actual_profit = self.get_actual_profit()  # 实际已成交收益
+        ##
+        # self.closing_value # 平仓挂单BTC总价
+        # self.closing_cost # 平仓挂单BTC成本
+        # self.expected_profit: float = 0  # 期望未成交收益 = 平仓挂单BTC总价 - 平仓挂单BTC成本
+        # self.expected_holding_value: float = 0  # 期望持仓市值 = 平仓挂单BTC总价
+        # self.expected_total_value: float = 0  # 期望总资产 = 现金余额 + 期望持仓市值（平仓挂单BTC总价）
+        # self.not_yet_achieved_profit: float = 0  # 尚未达到的收益 = 平仓挂单BTC总价 - 持仓市值
+        # self.actual_profit: float = 0  # 实际已成交收益 = 已成交的平仓挂单BTC总价 - 已成交的平仓挂单BTC成本
+        return f"{self.super_strategy.data.datetime.date()} {self.super_strategy.data.datetime.time()}\tBTC价格:{self.get_price():.2f}\t现金余额:{self.get_cash():.2f}\t持仓BTC数量:{self.get_position_size():.4f}\t持仓市值:{self.get_holding_value():.2f}\t期望持仓市值:{expected_holding_value:.2f}\t总资产:{self.get_total_value():.2f}\t期望总资产:{expected_total_value:.2f}\t期望未成交收益:{expected_profit:.2f}\t实际已成交收益:{actual_profit:.2f}\t平仓挂单BTC总价:{self.closing_value:2f}\t平仓挂单BTC成本:{self.closing_cost:2f}"
 
     def update_param(self):
         """
@@ -285,6 +417,11 @@ class MakerOnlyLongOnlyVolatilityStrategy(StrategyInterface):
                 open_array_length=self.OPEN_PRICE_SLOT_NUM, close_array_length=self.CLOSE_PRICE_SLOT_NUM,
                 direction=direction, commission_rate=self.commission_rate
             )
+            # 清除所有订单
+            open_orders = self.super_strategy.broker.get_orders_open()
+            if open_orders:
+                for order in open_orders:
+                    self.super_strategy.cancel(order)
 
     def get_price(self):
         """获取当前BTC价格"""
@@ -306,6 +443,41 @@ class MakerOnlyLongOnlyVolatilityStrategy(StrategyInterface):
         """当前持仓币量"""
         return self.super_strategy.position.size
 
+    def get_expected_profit(self) -> float:
+        """
+        用于分析
+        期望未成交收益 = 平仓挂单BTC总价 - 平仓挂单BTC成本
+        """
+        return self.closing_value - self.closing_cost
+
+    def get_expected_holding_value(self) -> float:
+        """
+        用于分析
+        期望持仓市值 = 平仓挂单BTC总价
+        """
+        return self.closing_value
+
+    def get_expected_total_value(self) -> float:
+        """
+        用于分析
+        期望总资产 = 现金余额 + 期望持仓市值（平仓挂单BTC总价）
+        """
+        return self.get_cash() + self.closing_value
+
+    def get_not_yet_achieved_profit(self) -> float:
+        """
+        用于分析
+        尚未达到的收益 = 平仓挂单BTC总价 - 持仓市值
+        """
+        return self.closing_value - self.get_holding_value()
+
+    def get_actual_profit(self) -> float:
+        """
+        用于分析
+        实际已成交收益 = 已成交的平仓挂单BTC总价 - 已成交的平仓挂单BTC成本
+        """
+        return self.closing_value_finished - self.closing_cost_finished
+
 
 class MyOrderPairObserver:
     """
@@ -313,6 +485,7 @@ class MyOrderPairObserver:
     观察MyOrderPair
     持有bt.Order
     """
+
     def __init__(self, strategy: bt, bt_order: bt.Order):
         self.strategy = strategy
         self.bt_order = bt_order
@@ -337,22 +510,23 @@ class MyOrderPairObserver:
             # 新建挂单
             if is_buy:
                 self.bt_order = self.strategy.buy(
-                        price=order_price,
-                        size=quantity,
-                        exectype=bt.Order.Limit
+                    price=order_price,
+                    size=quantity,
+                    exectype=bt.Order.Limit
                 )
                 # logging.info(f"Order Placed\tDirection: Buy,\tPrice: {self.bt_order.price},\tSize: {self.bt_order.size}")
             else:
                 self.bt_order = self.strategy.sell(
-                        price=order_price,
-                        size=quantity,
-                        exectype=bt.Order.Limit
+                    price=order_price,
+                    size=quantity,
+                    exectype=bt.Order.Limit
                 )
                 # logging.info(f"Order Placed\tDirection: Sell,\tPrice: {self.bt_order.price},\tSize: {self.bt_order.size}")
 
     def check_value(self, value: float):
         if value < 0:
             raise ValueError(f'value must be >= 0, not {value}')
+
 
 class StrategyAdaptor:
     """
@@ -363,6 +537,7 @@ class StrategyAdaptor:
     2.接受MyOrderPair的notify信号，并修改backtrader.Order的价格
     3.接受backtrader.Order的变更信号 并修改MyOrderPair的状态
     """
+
     def __init__(self):
         # 正向绑定
         self.bindings: Dict[MyOrderPair, MyOrderPairObserver] = {}
